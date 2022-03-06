748d18fa9116465bae2525f160fa83137868603f
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2022-03-06 17:19:43 -0600

    assignment 4 - complete

--- assignment4/WalmartLogin/app/src/main/java/edu/miu/walmartlogin/ui/MainActivity.kt
+++ assignment4/WalmartLogin/app/src/main/java/edu/miu/walmartlogin/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package edu.miu.walmartlogin.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import edu.miu.walmartlogin.data.User
 import edu.miu.walmartlogin.databinding.ActivityMainBinding
 
@@ -23,15 +26,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val user: User? = intent.getSerializableExtra("user") as User?
+        user?.let { users.add(it) }
     }
 
     fun onSign(view: View) {
         val email = binding.etEmail.text.toString().trim()
         val pass = binding.etPassword.text.toString().trim()
+        if(email.isEmpty()){
+            Toast.makeText(applicationContext,"Please enter your email", Toast.LENGTH_LONG).show()
+            return
+        }
+        if(pass.isEmpty()){
+            Toast.makeText(applicationContext,"Please enter your password", Toast.LENGTH_LONG).show()
+            return
+        }
+        var isUserFound = false
         for(user in users){
             if(user.username == email && user.password == pass){
+                isUserFound = true
                 openShoppingCategoryActivity(user)
             }
+        }
+        if(!isUserFound){
+            Toast.makeText(applicationContext,"Username and password not found.", Toast.LENGTH_LONG).show()
         }
     }
 
@@ -41,5 +59,22 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    fun onSignUp(view: View) {
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    fun onForgetPasswordClick(view: View) {
+        val email = binding.etEmail.text.toString().trim()
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_EMAIL, email)
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Forgot Password from MDP Course")
+        intent.putExtra(Intent.EXTRA_TEXT, "Forgot Password from MDP Course, " +
+                "for resetting use this code: 123456.");
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
 
 }
