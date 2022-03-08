b4954e409ab4f79ce87013f1adf0d8ef68389b69
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2022-03-07 21:55:16 -0600

    assignment 5 - problem 1 - quizapp

---
+++ assignment5/QuizApp/app/src/main/java/edu/miu/quizapp/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package edu.miu.quizapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.miu.quizapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
