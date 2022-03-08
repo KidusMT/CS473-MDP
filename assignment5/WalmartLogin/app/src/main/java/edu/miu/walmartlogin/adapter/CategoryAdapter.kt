30af09cd9cfcc89955418001051c001a2eef0f0a
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2022-03-07 21:37:34 -0600

    assignment 5 - screenshots pictures and pdf

--- assignment5/WalmartLogin/app/src/main/java/edu/miu/walmartlogin/adapter/CategoryAdapter.kt
+++ assignment5/WalmartLogin/app/src/main/java/edu/miu/walmartlogin/adapter/CategoryAdapter.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import edu.miu.walmartlogin.R
 import edu.miu.walmartlogin.data.Product
--- assignment5/WalmartLogin/app/src/main/java/edu/miu/walmartlogin/ui/ElectronicsCategoryActivity.kt
+++ assignment5/WalmartLogin/app/src/main/java/edu/miu/walmartlogin/ui/ElectronicsCategoryActivity.kt
@@ -1,5 +1,6 @@
 package edu.miu.walmartlogin.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +14,10 @@ class ElectronicsCategoryActivity : AppCompatActivity(), CategoryAdapter.Callbac
 
     private lateinit var binding: ActivityElectronicsItemBinding
     private val productList = mutableListOf(
-        Product("HP Flyer Red 15.6 \" Laptop", "$435", "Red", R.drawable.laptop, 1236L, "Nice laptop."),
-        Product("RCA Voyager 7\" 16GB android tablet", "$35", "Blue", R.drawable.tablet, 123L, "Nice tablet."),
-        Product("Vizio 70\" class 4K", "$235", "Black", R.drawable.tv, 1234L, "Nice TV Screen."),
-        Product("EPSON workforce WF-2750", "$135", "Black", R.drawable.scanner, 1235L, "Nice Scanner.")
+        Product("HP Flyer Red 15.6 \" Laptop", "$435", "Red", R.drawable.laptop, 1236L, "Nice laptop Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the "),
+        Product("RCA Voyager 7\" 16GB android tablet", "$35", "Blue", R.drawable.tablet, 123L, "Nice tablet Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the "),
+        Product("Vizio 70\" class 4K", "$235", "Black", R.drawable.tv, 1234L, "Nice TV Screen Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the ."),
+        Product("EPSON workforce WF-2750", "$135", "Black", R.drawable.scanner, 1235L, "Nice Scanner Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the .")
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +32,8 @@ class ElectronicsCategoryActivity : AppCompatActivity(), CategoryAdapter.Callbac
     }
 
     override fun onItemClicked(product: Product?) {
-        Toast.makeText(this, "${product?.title} Clicked", Toast.LENGTH_LONG).show()
+        val intent = Intent(this, ElectronicsDetailActivity::class.java)
+        intent.putExtra("product", product)
+        startActivity(intent)
     }
 }
